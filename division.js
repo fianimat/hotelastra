2461905abc0ec52a95a3c49307a202c0b98924fa
Zachary Connex <31832632+fianimat@users.noreply.github.com>
2018-10-18 14:16:04 -0400

    Add files via upload

---
+++ division.js
@@ -0,0 +1,24 @@
+function divone() {
+document.getElementById('done').style.display="block";
+document.getElementById('dtwo').style.display="none";
+document.getElementById('dthree').style.display="none";
+document.getElementById('dfour').style.display="none";
+}
+function divtwo() {
+document.getElementById('done').style.display="none";
+document.getElementById('dtwo').style.display="block";
+document.getElementById('dthree').style.display="none";
+document.getElementById('dfour').style.display="none";
+}
+function divthree() {
+document.getElementById('done').style.display="none";
+document.getElementById('dtwo').style.display="none";
+document.getElementById('dthree').style.display="block";
+document.getElementById('dfour').style.display="none";
+}
+function divfour() {
+document.getElementById('done').style.display="none";
+document.getElementById('dtwo').style.display="none";
+document.getElementById('dthree').style.display="none";
+document.getElementById('dfour').style.display="block";
+}
